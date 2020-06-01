45b3d73842658700c1bcef906c40d5766faeb470
rvaughan62 <robin@spellboundsoftware.com>
2020-06-01 12:43:49 -0400

    Modules for real-time collaboration

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -57,106 +57,11 @@ import Handlebars from './handlebars';
 import SbpAutoformat from './sbpautoformat';
 import ReactPluginEditing from './react-plugin-editing';
 
+import { customBackgroundColorPalette, customBorderColorPalette, customFontColorPalette } from './sbp-colors';
+
 import './custom.css';
 
 export default class ClassicEditor extends ClassicEditorBase {}
-
-const customBackgroundColorPalette = [
-	{
-		color: '#FFFFFF',
-		label: 'SB White'
-	},
-	{
-		color: '#1C48B4',
-		label: 'SB Blue'
-	},
-	{
-		color: '#4a86e8',
-		label: 'SB Mid Blue'
-	},
-	{
-		color: '#19b49b',
-		label: 'SB Green'
-	},
-	{
-		color: '#d9ead3',
-		label: 'SB Light Green'
-	},
-	{
-		color: '#999999',
-		label: 'SB Grey'
-	},
-	{
-		color: '#b7b7b7',
-		label: 'SB Light Grey'
-	},
-	{
-		color: '#ff9900',
-		label: 'SB Orange'
-	},
-	{
-		color: '#FF0000',
-		label: 'SB Red'
-	},
-	{
-		color: '#ea9999',
-		label: 'SB Mid Red'
-	},
-	{
-		color: '#f4cccc',
-		label: 'SB Light Red'
-	},
-	{
-		color: '#9900ff',
-		label: 'SB Purple'
-	}
-];
-
-const customBorderColorPalette = [
-	{
-		color: '#000000',
-		label: 'SB Black'
-	},
-	{
-		color: '#FFFFFF',
-		label: 'SB White'
-	},
-	{
-		color: '#999999',
-		label: 'SB Grey'
-	},
-	{
-		color: '#b7b7b7',
-		label: 'SB Light Grey'
-	}
-];
-
-const customFontColorPalette = [
-	{
-		color: '#000000',
-		label: 'SB Black'
-	},
-	{
-		color: '#FFFFFF',
-		label: 'SB White'
-	},
-	{
-		color: '#1C48B4',
-		label: 'SB Blue'
-	},
-	{
-		color: '#ff9900',
-		label: 'SB Orange'
-	},
-	{
-		color: '#FF0000',
-		label: 'SB Red'
-	},
-	{
-		color: '#9900ff',
-		label: 'SB Purple'
-	}
-];
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
---
+++ src/sbp-colors.js
@@ -0,0 +1,97 @@
+
+export const customBackgroundColorPalette = [
+	{
+		color: '#FFFFFF',
+		label: 'SB White'
+	},
+	{
+		color: '#1C48B4',
+		label: 'SB Blue'
+	},
+	{
+		color: '#4a86e8',
+		label: 'SB Mid Blue'
+	},
+	{
+		color: '#19b49b',
+		label: 'SB Green'
+	},
+	{
+		color: '#d9ead3',
+		label: 'SB Light Green'
+	},
+	{
+		color: '#999999',
+		label: 'SB Grey'
+	},
+	{
+		color: '#b7b7b7',
+		label: 'SB Light Grey'
+	},
+	{
+		color: '#ff9900',
+		label: 'SB Orange'
+	},
+	{
+		color: '#FF0000',
+		label: 'SB Red'
+	},
+	{
+		color: '#ea9999',
+		label: 'SB Mid Red'
+	},
+	{
+		color: '#f4cccc',
+		label: 'SB Light Red'
+	},
+	{
+		color: '#9900ff',
+		label: 'SB Purple'
+	}
+];
+
+export const customBorderColorPalette = [
+	{
+		color: '#000000',
+		label: 'SB Black'
+	},
+	{
+		color: '#FFFFFF',
+		label: 'SB White'
+	},
+	{
+		color: '#999999',
+		label: 'SB Grey'
+	},
+	{
+		color: '#b7b7b7',
+		label: 'SB Light Grey'
+	}
+];
+
+export const customFontColorPalette = [
+	{
+		color: '#000000',
+		label: 'SB Black'
+	},
+	{
+		color: '#FFFFFF',
+		label: 'SB White'
+	},
+	{
+		color: '#1C48B4',
+		label: 'SB Blue'
+	},
+	{
+		color: '#ff9900',
+		label: 'SB Orange'
+	},
+	{
+		color: '#FF0000',
+		label: 'SB Red'
+	},
+	{
+		color: '#9900ff',
+		label: 'SB Purple'
+	}
+];
